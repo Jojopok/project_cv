9f5a9d2d9fb02eec52434165cd17835132ebca50
Martha Smith <m.e.smith@live.co.uk>
2023-09-27 14:33:29 +0200

    added variables

---
+++ _competences.php
@@ -0,0 +1,11 @@
+
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>CV Groot</title>
+    <link rel="stylesheet" href="assets/css/variables.css">
+<h2>Competences</h2>
+
+<p>Hellooo</p>
