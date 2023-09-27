d960c7e8cd0b9e1b4affe9d2a469b1d11ea877e0
JONATHAN ZAIA <ZAIAJONATHAN@gmail.com>
2023-09-27 10:35:17 +0200

    add di and includ for diferent parties

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>CV Groot</title>
+    <link rel="stylesheet" href="assets/css/style.css">
   </head>
   <body>
     <header>
@@ -12,11 +13,35 @@
       ?>
     </header>
     <main>
-      
+      <div>
+      <?php
+       include "_profil.php"; 
+      ?>
+      </div>
+      <div>
+      <?php
+       include "_competences.php"; 
+      ?>
+      </div>
+      <div>
+      <?php
+       include "_experiences.php"; 
+      ?>
+      </div>
+      <div>
+      <?php
+       include "_formation.php"; 
+      ?>
+      </div>
+      <div>
+      <?php
+       include "_loisirs.php"; 
+      ?>
+      </div>
     </main>
     <footer>
     <?php
-       include "footer.php"; 
+       include "_footer.php"; 
       ?>
     </footer>
   </body>
