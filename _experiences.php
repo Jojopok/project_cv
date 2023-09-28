f19d93c0f2de65a118c1616d69d57c184559f2f5
Pagot Marion <marion.pagot@gmail.com>
2023-09-28 14:41:55 +0200

    competences ok en mobile

--- _experiences.php
+++ _experiences.php
@@ -3,36 +3,35 @@
 <img class="photog" src="assets/images/grootExpe.jpeg" alt="Photo de Groot"/>
 </div>
 <div class="titreExp">
-    Expérience
+    Expériences
 </div>
 <div class="divtext">
+
    
-   <h3>2023-présent</h3> 
-    <br><br>
+   <h3> 🌟2023-présent</h3> 
+    <br>
 <p>
-Rôle : Gardien de la Galaxie<br>
-Entreprise : Les Gardiens de la Galaxie<br>
-Lieu :  Partout dans l'univers<br><br>
+RÔLE : Gardien de la Galaxie<br>
+ENTREPRISE : Les Gardiens de la Galaxie<br>
+LIEU : Partout dans l'univers<br><br>
 
-
-Missions : <br>
+MISSIONS <br>
  > Protéger l'univers des menaces <br>
  > Combattre les forces du mal <br>
  > Aider les innocents <br>
  <br><br>
  </p>
 
-<h3>2014-2022</h3>
+<h3> 🌟2014-2022</h3> <br>
 <p>
-<br>
-Rôle : Soldat <br>
-Entreprise : L'Empire Kree <br>
-Lieu : Partout dans l'univers <br>
+RÔLE : Soldat <br>
+ENTREPRISE : L'Empire Kree <br>
+LIEU : Partout dans l'univers <br>
 <br><br>
-Missions :<br>
-
+MISSIONS <br>
 > Combattre les ennemis de l'Empire<br>
 > Protéger les intérêts de l'Empire<br>
 > Soutenir le règne de Ronan l'Accusateur <br> </p>
+
 
 </div>
