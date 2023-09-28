541a3a86ff2cf56481e0b538d7addb439cd2c398
Martha Smith <m.e.smith@live.co.uk>
2023-09-28 09:50:00 +0200

    added the list of skills

--- _competences.php
+++ _competences.php
@@ -13,6 +13,16 @@
 <div class="titre-competences">
     <h2>Compétences</h2>
 </div>
+<div class="skillbox">
+  <ul>
+<li>Jardinage</li>
+<li>Botaniste</li>
+<li>Regeneriste</li>
+<li>Comedie</li>
+<li>Management</li>
+<li>Linguistiques</li>
+  </ul>  
+</div>
 
 
 
