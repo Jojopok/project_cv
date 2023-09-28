53edb2be3575d5289b63668d00cf7f919af6da7d
Dju0 <julienginestal@gmail.com>
2023-09-28 15:54:52 +0200

    add bloc for loisir

--- _loisirs.php
+++ _loisirs.php
@@ -1,15 +1,23 @@
 <h2 class=titreloisir>Loisirs</h2>
 
 
-<div class="photo">
+<div class="photoboxloisir">
 <img class="photogrootloisir" src="assets/images/Groot_loisirs.jpg" alt="Photo Loisirs Groot"/>
 </div>
 
-<section class="loisir">
-
-<h1<img class=" photogrootdance" src ="assets/images/Groot_dance.jpg">Danse</h1> 
-<h1> Tir Sportif </h1>
-<h1> Jeux Vidéos </h1>
-<h1> Sieste </h1>
+<section 
+class="loisirs">
+<loisir>
+    <h3><loisir1>Danse</loisir1></h3>
+</loisir>
+<loisir>
+    <h3><loisir2>Tir Sportif</loisir2></h3>
+</loisir>
+<loisir>
+    <h3><loisir3>Jeux Vidéos</loisir3></h3>
+</loisir>
+<loisir>
+    <h3><loisir4>Sieste</loisir4></h3>
+</loisir>
 
 </section>
