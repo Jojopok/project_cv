27a47ae71c5cfd3c3536044fa70e3070aa4e3ba7
Dju0 <julienginestal@gmail.com>
2023-09-28 10:37:48 +0200

    add modifications on loisirs.css and .php

--- _loisirs.php
+++ _loisirs.php
@@ -2,7 +2,7 @@
 
 
 <div class="photobox">
-<img class="photogroot" src="assets/images/Groot Loisirs.jpg" alt="Photo Loisirs Groot"/>
+<img class="photogroot" src="assets/images/Groot_loisirs.jpg" alt="Photo Loisirs Groot"/>
 </div>
 
 <p> Danse </p>
