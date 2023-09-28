f0cbcbc226a21bd155bb620c3948f38d9d7d819e
JONATHAN ZAIA <ZAIAJONATHAN@gmail.com>
2023-09-28 11:36:42 +0200

    add

--- _competences.php
+++ _competences.php
@@ -1,14 +1,5 @@
-
-
-<!DOCTYPE html>
-<html lang="fr">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>CV Groot</title>
-    <link rel="stylesheet" href="assets/css/competences.css">
-    </div class="photobox">
-    <img class="photogroot" src="assets/images/GrootComp.jpg"/>
+</div class="photobox">
+<img class="photogroot" src="assets/images/GrootComp.jpg"/>
 </div>
 <div class="titre-competences">
     <h2>Compétences</h2>
@@ -23,6 +14,3 @@
 <li>Linguistiques</li>
   </ul>  
 </div>
-
-
-
---
+++ _experiences.php
@@ -0,0 +1,33 @@
+<div class="photob"></div>
+<img class="photog" src="images/GrootExpe.jpg" alt="Photo de Groot"/>
+</div>
+<div class="titreExp">
+    <ul>
+    <li>Expérience</li>
+    </ul>
+</div>
+<div class="textexp">
+    <p>  <br><br>
+    "2023-présent
+
+Rôle : Gardien de la Galaxie
+Entreprise : Les Gardiens de la Galaxie
+Lieu :  Partout dans l'univers
+
+Missions :
+ > Protéger l'univers des menaces
+ > Combattre les forces du mal
+ > Aider les innocents
+
+2014-2022
+
+Rôle : Soldat
+Entreprise : L'Empire Kree
+Lieu : Partout dans l'univers
+
+Missions :
+
+> Combattre les ennemis de l'Empire
+> Protéger les intérêts de l'Empire
+> Soutenir le règne de Ronan l'Accusateur"</p>
+</div>
