795907e60a384c5f46af44ec2113e7ab955093bb
Dju0 <julienginestal@gmail.com>
2023-09-28 10:06:12 +0200

    add modifications on loisirs.css and .php

---
+++ _loisirs.php
@@ -0,0 +1,11 @@
+<h2 class=titre>Loisirs</h2>
+
+
+<div class="photobox">
+<img class="photogroot" src="assets/images/Groot Loisirs.jpg" alt="Photo Loisirs Groot"/>
+</div>
+
+<p> Danse </p>
+<p> Tir Sportif </p>
+<p> Jeux Vidéos </p>
+<p> Sieste </p>
