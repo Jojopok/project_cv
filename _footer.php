66e7956ecad8f282ef21b05709fb2523514b4133
severine vanleene <severine.vanleene@gmail.com>
2023-10-03 21:50:25 +0200

    MAJfooter

--- _footer.php
+++ _footer.php
@@ -1,15 +1,37 @@
 <footer>
-<div class="divtextfooter">
-    <p>  <h4>adress</h4><br>
-     PLanet Kree 
-    </p>
-    <p><h4>phone</h4><Br>
-    4559879
-</p>
-<p><h4>Email adress</h4><br>
-     Groot@Grootworld </p>
+  <div class = "footerBase"> 
+    <div class ="footerContainer">
 
-</div>
+      <div class ="footerColonne">
+        
+        <div class="footerContainer">
+          <img src="assets/images/footer1.jpg" alt="" widh="50" height="50">
+          <h2>adresse</h2>
+        </div>
+        <p>PLanet KREK, rue PC </p> 
+      </div>
+      <div class ="footerColonne">
+        <h2>Téléphone</h2>
+        <p>6776"(-ç///3)" </p>
+      </div>
+      <div class ="footerColonne">
+        <h2>Email</h2>
+        <p>GrootCV@groot.world</p>
+      </div>
+    </div>
+    <div class ="footerContainer">
+      <div class ="footerColonne">
+      <a href="https://www.instagram.com/explore/tags/groot/?hl=fr"><img src="assets/images/instalogo.jpg" alt=""widh="70" height="70"></a>
+      </div>
+      <div class ="footerColonne">
+        <p>#GROOT</P> <p>#</p>#Botaniste </p>
+      </div>
+      <div class ="footerColonne">
+        <a href="top">Backtotop</a>
+      </div>
+    </div>
+  </div>
+</footer>
 
    
 
