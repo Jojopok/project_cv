3eefc359dc6cfb5c7bd0a3febcb962b2f89d7512
Jojopok <zaiajonathan@gmail.com>
2023-10-04 22:48:07 +0200

    Add a smoth slid bar in js

--- _navBar.php
+++ _navBar.php
@@ -1,13 +1,15 @@
 <div class="navbarcontainer">
 <nav class="navbar">
       <ul>
-        <li><a href="#competences">Compétences</a></li>
-        <li><a href="#experiences">Expériences</a></li>
-        <li><a href="#formation">Formation</a></li>
-        <li><a href="#loisirs">Loisirs</a></li>
+        <li><a id="competences2" >Compétences</a></li>
+        <li><a id="experiences2">Expériences</a></li>
+        <li><a id="formation2">Formation</a></li>
+        <li><a id="loisirs2">Loisirs</a></li>
       </ul>
     </nav>
     <img src="assets/images/hamb.png" alt="menu hamburger" class="menuhamburger">
 
 </div>
+
+
 
