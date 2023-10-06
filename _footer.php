78ef6ee6f2e332d95d8e6bd71287b71ccef1f7f2
severine vanleene <severine.vanleene@gmail.com>
2023-10-06 11:56:20 +0200

    testes footer

--- _footer.php
+++ _footer.php
@@ -1,4 +1,5 @@
-<footer>
+
+
   <div class = "footerBase"> 
     <div class ="footerContainer">
 
@@ -11,12 +12,11 @@
         <p>PLanet KREK, rue PC </p> 
       </div>
       <div class ="footerColonne">
-      <div class="footerContainer">
-      <img src="assets/images/phone.png" alt="" widh="50" height="50">  
-      <span>Téléphone</span>
-    
- </div>
-      <p>055657898908 </p>
+       <div class="footerContainer">
+        <img src="assets/images/phone.png" alt="" widh="50" height="50">  
+          <span>Téléphone</span>
+        </div>
+         <p>055657898908 </p>
       </div>
       <div class ="footerColonne">
       <div class="footerContainer">
@@ -27,22 +27,17 @@
       </div>
     </div>
     <div class ="footerContainer">
-      <di class ="footerColonne">
-      
-    <a href="https://www.instagram.com/babygrootbabygroot/"><img src="assets/images/insta2.png" alt=""widh="100" height="100"></a>
-        
-  </di>
       <div class ="footerColonne">
-      <a href="https://www.gifimili.com/images-les-gardiens-de-la-galaxie/" target="_blank"><img src="https://www.gifimili.com/gif/2018/05/groot-danse.gif" border="0" alt="Groot danse"></a>
-        
-
+      <a href="https://www.instagram.com/babygrootbabygroot/"><img src="assets/images/insta2.png" alt=""widh="100" height="100"></a>
+    </div>
+      <div class ="footerColonne">
+        <a href="https://www.gifimili.com/images-les-gardiens-de-la-galaxie/" target="_blank"><img class="footergif" src="https://www.gifimili.com/gif/2018/05/groot-danse.gif"  alt="Groot danse"></a>
       </div>
       <div class ="footerColonne">
         <a href="top"><img src="assets/images/flechefooter.png" alt=""widh="70" height="70"></a>
       </div>
     </div>
   </div>
-</footer>
 
    
 
